e022475590bf7cfff5a908bd501890586816db13
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-05-08 11:06:43 +0530

    Update 2.0

--- StaffSwap/app/src/main/java/com/example/staffswap/AddTimeTableActivity.java
+++ StaffSwap/app/src/main/java/com/example/staffswap/AddTimeTableActivity.java
@@ -1,14 +1,22 @@
 package com.example.staffswap;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.dynamicanimation.animation.SpringAnimation;
+
+import java.util.ArrayList;
 
 public class AddTimeTableActivity extends AppCompatActivity {
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,36 @@ public class AddTimeTableActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        spinner = findViewById(R.id.AddTimeTableSpinner);
+
+        ArrayList<String> leaveTypes = new ArrayList<>();
+        leaveTypes.add("Select Day ---");
+        leaveTypes.add("Monday");
+        leaveTypes.add("Tuesday");
+        leaveTypes.add("Wednesday");
+        leaveTypes.add("Thursday");
+        leaveTypes.add("Friday");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                leaveTypes
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedLeave = parent.getItemAtPosition(position).toString();
+                // Do something with selectedLeave
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Handle no selection
+            }
+        });
     }
+
 }
