775e1f1d0bf1088112cf91b59fafe8c1db0f6c28
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-05-08 12:37:53 +0530

    Update

--- StaffSwap/app/src/main/java/com/example/staffswap/UserLoginActivity.java
+++ StaffSwap/app/src/main/java/com/example/staffswap/UserLoginActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.example.staffswap.model.CustomAlert;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +15,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class UserLoginActivity extends AppCompatActivity {
+
+    EditText NIC,Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +27,28 @@ public class UserLoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        }); 
+        });
+
+        NIC = findViewById(R.id.UserLoginText01);
+         Password = findViewById(R.id.UserLoginText02);
+
         Button loginButton = findViewById(R.id.UserLoginBtn);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(UserLoginActivity.this, AddTimeTableActivity.class);
-                startActivity(intent);
+
+               String nic = NIC.getText().toString();
+                String  password = Password.getText().toString();
+                if(nic.isEmpty()){
+                    CustomAlert.showCustomAlert(UserLoginActivity.this, "Error !", "Please Fill NIC", R.drawable.cancel);
+                } else if (password.length() > 5) {
+                    Intent intent = new Intent(UserLoginActivity.this, UserHomeActivity.class);
+                    startActivity(intent);
+
+                }
             }
         });
+
+
     }
 }
